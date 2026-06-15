451d8abd599d88bb6f33df41c25c88d54fa87892
miliiiex <malakelboukili258@gmail.com>
2026-06-15 17:07:37 -0400

    Clean : Suppression du fichier temporaire test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from modules.host_discovery import discover_hosts
-from modules.port_scanner import scan_ports
-from modules.vuln_checker import check_vulns 
-hosts=discover_hosts('192.168.0.0/24')
-for host in hosts:
-	ports=scan_ports(host)
-	vulns=check_vulns(ports)
